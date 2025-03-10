5c455259a3357f1d8940eb679a810b40fb2e94ec
Jikkan <69170077+Twilighttsky@users.noreply.github.com>
2025-03-10 15:16:02 -0300

    ATT
    Criação da Tela de Login (estrutura e coloração base)

--- busca-pet/src/App.jsx
+++ busca-pet/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Login from "./pages/tela_login/login";
+
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Login />
+    </>
+  )
+  
 }
 
 export default App;
