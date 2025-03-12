31638bcfd138c7beb4c6fef37938e72ebf29029b
Jikkan <69170077+Twilighttsky@users.noreply.github.com>
2025-03-12 15:38:00 -0300

    att
    Foram adicionados parametros de estilização e variaveis

--- busca-pet/src/pages/tela_login/Login.jsx
+++ busca-pet/src/pages/tela_login/Login.jsx
@@ -1,37 +1,45 @@
-import Logo from './imgs/logo.png'
+import Logo from "./imgs/logo.png";
 import "./Style.css";
 
 function login() {
-return <>
-    
-       
-            <header>
-                <img src={Logo} alt="cachorro olhando por tras de uma lupa"/>
-                <h1>BuscaPet</h1>
-            </header>
+  return (
+    <>
+      <header>
+        <img src={Logo} alt="cachorro olhando por tras de uma lupa" />
+        <h1>BuscaPet</h1>
+      </header>
 
+      <div className="conjuntoEsquerdo">
+        <h2>
+          Bem-vindo de volta!<br></br>Vamos encontrar mais pets juntos?
+        </h2>
 
-            <div className="conjuntoEsquerdo">
-            <h2>Bem-vindo de volta!<br></br>Vamos encontrar mais pets juntos?</h2>
-            
-            <form action="">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" placeholder='Digite a sua senha'></input>
-                    <label htmlFor="senha">Senha</label>
-                    <input type="text" placeholder='Digite a sua senha'></input>
-                    <button>Login</button>
-            </form>
-        
+        <form action="">
 
-            <p>Ainda não tem cadastro? <a href="">Cadastre-se!</a></p>
-            <p>Esqueceu a senha? <a href="">Recupere-a!</a></p>
-            </div>
+          <div className="campos">
+            <label htmlFor="email">Email</label>
+            <input type="text" placeholder="Digite a sua senha"></input>
+          </div>
 
-        
+          <div className="campos">
+            <label htmlFor="senha">Senha</label>
+            <input type="text" placeholder="Digite a sua senha"></input>
+          </div>
 
+          <button>Login</button>
+        </form>
 
-   
-</>;
+        <div className="redirect">
+          <p>
+            Ainda não tem cadastro? <a href="">Cadastre-se!</a>
+          </p>
+          <p>
+            Esqueceu a senha? <a href="">Recupere-a!</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default login;
