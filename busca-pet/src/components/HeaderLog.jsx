943b30fd54fa7461a1abf1a2387aa9c13a0eb89d
sabrina1405 <sabrina.s.souzarosa@gmail.com>
2025-03-13 01:16:10 -0300

    Adição do conteúdo header logado + estilização
    adição dos botões do header;
    criação das variáveis de cores a serem utilizadas;

--- busca-pet/src/components/HeaderLog.jsx
+++ busca-pet/src/components/HeaderLog.jsx
@@ -1,12 +1,17 @@
    import Logo_Cachorro from "./../imgs/Logo_Cachorro.png"
    import Icone from "./../imgs/Icone.png"
-   
+
    function HeaderLog(){
         return <div>
             <header>
-            <img src={Logo_Cachorro} alt="Logo com um cachorro peludo usando uma lupa" width="500px"/>
-            <img src={Icone} alt="Icone de usuário sem foto" />
+            <img className="logodog" src={Logo_Cachorro} alt="Logo com um cachorro peludo usando uma lupa" width="500px"/>
+            <img className="icon" src={Icone} alt="Icone de usuário sem foto" width="70px" height="70px" />
+            <button>Todos</button>
+            <button>Achados</button>
+            <button>Perdidos</button>
+            <button>Pesquisar</button>
             </header>
+
         </div>
     }
     export default HeaderLog
