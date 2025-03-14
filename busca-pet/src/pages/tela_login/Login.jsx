a0e7650772862f7d8d2b5165627ec0c125a2c339
Jikkan <69170077+Twilighttsky@users.noreply.github.com>
2025-03-14 13:53:00 -0300

    att
    vulnerabilidade consertada e responsividade ativa no formulario do canto esquerdo

--- busca-pet/src/pages/tela_login/Login.jsx
+++ busca-pet/src/pages/tela_login/Login.jsx
@@ -1,43 +1,45 @@
 import Logo from "./imgs/logo.png";
+import Gato from "./imgs/gato.png"
 import "./Style.css";
 
 function login() {
   return (
     <>
-      <header>
-        <img src={Logo} alt="cachorro olhando por tras de uma lupa" />
-        <h1>BuscaPet</h1>
-      </header>
+      <div className="main">
+        <header>
+          <img src={Logo} alt="cachorro olhando por tras de uma lupa" />
+          <h1>BuscaPet</h1>
+        </header>
+        <div className="conjuntoEsquerdo">
+          <h2>
+            Bem-vindo de volta!<br></br>Vamos encontrar mais pets juntos?
+          </h2>
 
-      <div className="conjuntoEsquerdo">
-        <h2>
-          Bem-vindo de volta!<br></br>Vamos encontrar mais pets juntos?
-        </h2>
+          <form action="">
 
-        <form action="">
+            <div className="campos">
+              <label htmlFor="email">Email</label>
+              <input type="text" placeholder="Digite a sua senha"></input>
+            </div>
+            <div className="campos">
+              <label htmlFor="senha">Senha</label>
+              <input type="text" placeholder="Digite a sua senha"></input>
+            </div>
+            <button>Login</button>
+          </form>
 
-          <div className="campos">
-            <label htmlFor="email">Email</label>
-            <input type="text" placeholder="Digite a sua senha"></input>
+          <div className="redirect">
+            <p>
+              Ainda não tem cadastro? <a href="">Cadastre-se!</a>
+            </p>
+            <p>
+              Esqueceu a senha? <a href="">Recupere-a!</a>
+            </p>
           </div>
-
-          <div className="campos">
-            <label htmlFor="senha">Senha</label>
-            <input type="text" placeholder="Digite a sua senha"></input>
-          </div>
-
-          <button>Login</button>
-        </form>
-
-        <div className="redirect">
-          <p>
-            Ainda não tem cadastro? <a href="">Cadastre-se!</a>
-          </p>
-          <p>
-            Esqueceu a senha? <a href="">Recupere-a!</a>
-          </p>
         </div>
       </div>
+
+
     </>
   );
 }
