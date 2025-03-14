a7df7d43aea93b613e35f89b2671b9786a87f971
sabrina1405 <sabrina.s.souzarosa@gmail.com>
2025-03-13 21:40:19 -0300

    Criação do conteúdo PageResearch + Estilização
    + Criação do conteúdo da Página de pesquisa;
    
    + Estilização do conteúdo;

--- busca-pet/src/App.jsx
+++ busca-pet/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import HeaderLog from "./components/HeaderLog";
+import PageResearch from "./components/PageResearch";
 
 function App() {
   return <>
-  <HeaderLog />
+  <PageResearch/>
   
   </>;
 }
--- busca-pet/src/components/PageResearch.jsx
+++ busca-pet/src/components/PageResearch.jsx
@@ -1,9 +1,45 @@
 import listagem from "./../imgs/listagem.png"
 import lupa_pesquisa from "./../imgs/lupa_pesquisa.png"
-
+import icon_cachorro from "./../imgs/icon_cachorro.png"
+import icon_location from "./../imgs/icon_location.png"
+import icon_excluir from "./../imgs/icon_excluir.png"
 function PageResearch(){
-    return <body>
-            <button><img src={listagem} alt="Ícone de listagem" />Pesquise por Bairro, Espécie, Data, Usar localização atual...<img src={lupa_pesquisa} alt="Ícone de lupa de pesquisa" /></button>
-        </body>
-    
+    return <div className="main">
+            
+        {/* Configurações para o input de pesquisa */}
+
+            <div className="search_container">
+                <button className="search_button">
+                <img src={listagem} alt="Ícone de listagem" className="list"/>
+                <input type="text" className=" search_input" placeholder="Pesquise por Bairro, Espécie, Data, Usar localização atual..."/>
+        
+                <img src={lupa_pesquisa} alt="Ícone de lupa de pesquisa" className="lupa" />
+                </button>
+            </div>
+
+            <div className="search_history">
+
+            <button className="search_location">
+                <img src={icon_location} alt="Ícone de localização" height="70px" />
+                <img src={lupa_pesquisa} alt="Ícone de lupa de pesquisa" className="lupa" width="80px"/>
+                Bairro das Cobras
+                <img src={icon_excluir} alt="Ícone de excluir do histórico" width="50px" />
+            </button>
+            
+            </div>
+
+            <div className="search_history">
+            <button className="search_dog">
+                <img src={icon_cachorro} alt="Ícone de cachorro"/>
+                <img src={lupa_pesquisa} alt="Ícone de lupa de pesquisa" className="lupa" width="80px" />
+                Cachorro Laranja
+                <img src={icon_excluir} alt="Ícone de excluir do histórico" width="50px"/>
+            </button>
+
+
+            </div>
+
+        </div>
 }
+
+export default PageResearch
