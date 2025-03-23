dc85872f9eab45b26b008c73f49478c18b1416f9
Israel de Souza <israellimas@hotmail.com>
2025-03-23 01:03:35 -0300

    feat: funcao que retorna a conexão com o banco

--- backend/model/connectionOracle.js
+++ backend/model/connectionOracle.js
@@ -1,11 +1,23 @@
-import dotenv from 'dotenv'
+import OracleDB from "oracledb";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 // informações que vão ficar no .env para só os devs terem acesso ao banco
 const dbConfig = {
-    user: dotenv.DB_USER,
-    password: dotenv.DB_PASSWORD,
-    connectString: dotenv.DB_STRING_CONNECT
+  user: dotenv.DB_USER,
+  password: dotenv.DB_PASSWORD,
+  connectString: dotenv.DB_STRING_CONNECT,
+};
+
+// uma função que vai retornar a conexão com o banco quando for chamada em outro arquivo
+async function getConnection() {
+  try {
+    return await OracleDB.getConnection(dbConfig);
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", err);
+    throw err;
+  }
 }
 
+export default getConnection;
