1d8204c42a8956293577e16581228f582971d675
Israel de Souza <israellimas@hotmail.com>
2025-03-25 18:25:41 -0300

    feat/refactor: criação de rota + delimitação escopo de estilização de recuperacao de senha

--- frontend/busca-pet/src/App.jsx
+++ frontend/busca-pet/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
+import RecuperacaoSenha from "./pages/RecuperacaoSenha";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/form/cadastro-usuario" element={<Cadastro />} />
           <Route path="/form/login" element={<Login />} />
+          <Route path="/form/recuperacao-senha" element={<RecuperacaoSenha />} />
+
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/busca-pet/src/pages/RecuperacaoSenha.jsx
+++ frontend/busca-pet/src/pages/RecuperacaoSenha.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import HeaderForm from "../components/HeaderForm";
 import InputTxt from "../components/InputTxt";
 import ButtonForm from "../components/ButtonForm";
+import style from './styles/recuperacao_senha.module.css'
 
 const RecuperacaoSenha = () => {
   return (
-    <div className="rec-senha">
-      <div className="rec-senha__form">
-        <div className="rec-senha__header">
+    <div className={style['rec-senha']}>
+      <div className={style['rec-senha__form']}>
+        <div className={style['rec-senha__header']}>
           <HeaderForm />
         </div>
 
-        <div className="rec-senha__body">
-          <div className="rec-senha__box-title">
-            <h2 className="rec-senha__title">
+        <div className={style['rec-senha__body']}>
+          <div className={style['rec-senha__box-title']}>
+            <h2 className={style['rec-senha__title']}>
               Esqueceu a senha? Vamos Recupera-la!
             </h2>
           </div>
 
-          <div className="rec-senha__box-form">
+          <div className={style['rec-senha__box-form']}>
             <InputTxt name="Email" place="Insira o seu email aqui" />
             <InputTxt
               name="Código de validação"
@@ -31,13 +32,15 @@ const RecuperacaoSenha = () => {
             />
           </div>
 
-          <div className="rec-senha__box-btn">
+          <div className={style['rec-senha__box-btn']}>
             <ButtonForm placeholder="Redefinir Senha" />
           </div>
         </div>
       </div>
 
-      <div className="rec-senha__image"></div>
+      <div className={style['rec-senha__image']}>
+
+      </div>
     </div>
   );
 };
