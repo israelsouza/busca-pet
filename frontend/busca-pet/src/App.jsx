2b0148720e66b293423c17af28c37e006de1be46
Israel de Souza <israellimas@hotmail.com>
2025-03-28 14:41:51 -0300

    chore: facilitação endpoint para dev

--- frontend/busca-pet/src/App.jsx
+++ frontend/busca-pet/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/form/cadastro" element={<Cadastro />} />
+          <Route path="/" element={<Cadastro />} />
           <Route path="/form/login" element={<Login />} />
           <Route path="/form/recuperacao-senha" element={<RecuperacaoSenha />} />
 
