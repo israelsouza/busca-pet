e5fb2b98e18d0df90254212b23590a9149f5e62c
Israel de Souza <israellimas@hotmail.com>
2025-03-31 01:24:29 -0300

    feat: update route for user registration form

--- frontend/busca-pet/src/App.jsx
+++ frontend/busca-pet/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Cadastro />} />
+          <Route path="/form/cadastro-usuario" element={<Cadastro />} />
           <Route path="/form/login" element={<Login />} />
           <Route path="/form/recuperacao-senha" element={<RecuperacaoSenha />} />
 
