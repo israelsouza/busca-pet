b5c7831500e13958b735ee8ebffa3e786f01e9f7
Israel de Souza <matheusgobbi50@gmail.com>
2025-04-01 13:23:39 -0300

    feat: criação de useRef e amarração aos campos

--- frontend/busca-pet/src/pages/PetPerdido.jsx
+++ frontend/busca-pet/src/pages/PetPerdido.jsx
@@ -5,26 +5,32 @@ import HeaderLog from "./../components/HeaderLog";
 
 function PetPerdido() {
 
-    // declarar useRef como null
-    // amarrar useRef nos campos
     // declarar useState
     // fazer as condições de erro
     // montar o objeto
     // enviar requisição ao backend
     
-    function validarDados() {
-        let nome = document.getElementById(nome);
-        let rga = document.getElementById(rga);
-        let descricao = document.getElementById(descricao);
-        let data = document.getElementById(data);
+
+    // function validarDados() {}
+
+        // let nome = document.getElementById(nome);
+        // let rga = document.getElementById(rga);
+        // let descricao = document.getElementById(descricao);
+        // let data = document.getElementById(data);
     
-        if (nome == "" || descricao == "" || data == "") {
-            alert("Preencha os campos obrigatórios");
-            return false;
-        }
+        // if (nome == "" || descricao == "" || data == "") {
+        //     alert("Preencha os campos obrigatórios");
+        //     return false;
+        // }
     
-        const informacaoPetPerdido = [nome, rga, descricao, data]
-    }
+        // const informacaoPetPerdido = [nome, rga, descricao, data]
+
+        const nomeRef = useRef(null);
+        const rgaRef = useRef(null);
+        const tipoPetRef = useRef(null);
+        const descricaoRef = useRef(null);
+        const dataRef = useRef(null);
+        const imagemRef = useRef(null);
 
     return (
         <div className={styles.pet_perdido}>
@@ -37,18 +43,18 @@ function PetPerdido() {
                     <div className={styles.pet_perdido__input50}>
                         <div>
                             <label htmlFor="">Nome do Pet</label>
-                            <input id="nome" className={styles.pet_perdido__input} type="text" />
+                            <input id="nome" refProp={nomeRef} className={styles.pet_perdido__input} type="text" />
                         </div>
 
                         <div>
                             <label htmlFor="">RGA do Pet (opcional)</label>
-                            <input id="rga" className={styles.pet_perdido__input} type="text" />
+                            <input id="rga" refProp={rgaRef} className={styles.pet_perdido__input} type="text" />
                         </div>
                     </div>
 
                     <div className={styles.pet_perdido__input100}>
                         <label htmlFor="">Selecione o tipo do seu Pet</label>
-                        <select className={styles.pet_perdido__input} name="" id="" >
+                        <select refProp={tipoPetRef} className={styles.pet_perdido__input} name="" id="" >
                             <option value="" disabled>Selecione o tipo do pet</option>
                             <option value="gato">Gato</option>
                             <option value="cachorro">Cachorro</option>
@@ -60,14 +66,14 @@ function PetPerdido() {
 
                     <div className={styles.pet_perdido__input100}>
                         <label htmlFor="">Breve descrição do Pet</label>
-                        <textarea id="descricao" className={`${styles.pet_perdido__input} ${styles.pet_perdido__textarea}`} ></textarea>
+                        <textarea id="descricao" refProp={descricaoRef} className={`${styles.pet_perdido__input} ${styles.pet_perdido__textarea}`} ></textarea>
                     </div>
 
                     Local onde o Pet foi encontrado - API Google Maps
                     
                     <div className={styles.pet_perdido__input100}>
                         <label htmlFor="">Data</label>
-                        <input id="data" className={styles.pet_perdido__input} type="date" />
+                        <input id="data" refProp={dataRef} className={styles.pet_perdido__input} type="date" />
                     </div>
 
                     Acrescentar uma forma do input mudar quando um arquivo for selecionado
@@ -75,11 +81,9 @@ function PetPerdido() {
                     <p className={styles.pet_perdido__imagem_texto}>Selecione a imagem do Pet perdido</p>
                     <div className={styles.pet_perdido__imagem}>
                         <label htmlFor="imagem">Selecionar arquivo</label>
-                        <input id="imagem" type="file"/>
+                        <input id="imagem" refProp={imagemRef} type="file"/>
                     </div>
                 </form>
-
-                Botão ta descentralizado
 
                 <button className={`${styles.botao} ${styles.perdeu}`} onClick={validarDados}>Cadastrar Pet Perdido</button>
             </div>
