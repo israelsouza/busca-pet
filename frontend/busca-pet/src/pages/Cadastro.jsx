283d1bcd60a76b34f461011bd33765767ff8628b
Israel de Souza <israellimas@hotmail.com>
2025-03-31 21:15:10 -0300

    refactor: melhoria na legibilidade das validações

--- frontend/busca-pet/src/pages/Cadastro.jsx
+++ frontend/busca-pet/src/pages/Cadastro.jsx
@@ -6,13 +6,13 @@ import Option from "../components/Option";
 import ButtonForm from "../components/ButtonForm";
 import { Link } from "react-router-dom";
 import {
-  verificarCampoVazio, 
-  verificarTamanhoMaximo, 
+  verificarCampoVazio,
+  verificarTamanhoMaximo,
   verificarTamanhoFixo,
   verificarTamanhoMinimo,
   verificarSeTemLetras,
   verificarSeTemNumeros,
-  verificarSeEEmail
+  verificarSeEEmail,
 } from "../assets/utils/formValidacoes";
 import enviarDados from "../assets/utils/enviarDados";
 
@@ -52,97 +52,176 @@ function Cadastro() {
     const cidade = cidadeRef.current;
     const estado = estadoRef.current;
 
-    
     // campos vazios
     const camposObrigatorios = [
-      {  ref: name,  setErro: setErroNome,  mensagem: "Por favor, insira seu nome." },
-      {  ref: email,  setErro: setErroEmail,  mensagem: "Por favor, insira seu e-mail." },
-      {  ref: senha,  setErro: setErroSenha,  mensagem: "Por favor, crie a sua senha." },
-      {  ref: phone,  setErro: setErroPhone,  mensagem: "Por favor, insira seu telefone." },
-      {  ref: rua,  setErro: setErroRua,  mensagem: "Por favor, insira o nome da sua rua." },
-      {  ref: bairro,  setErro: setErroBairro,  mensagem: "Por favor, insira o nome do seu bairro." },
-      {  ref: cep,  setErro: setErroCEP,  mensagem: "Por favor, insira seu CEP." },
-      {  ref: cidade,  setErro: setErroCidade,  mensagem: "Por favor, insira o nome da sua cidade." }
-    ]
+      {
+        ref: name,
+        setErro: setErroNome,
+        mensagem: "Por favor, insira seu nome.",
+      },
+      {
+        ref: email,
+        setErro: setErroEmail,
+        mensagem: "Por favor, insira seu e-mail.",
+      },
+      {
+        ref: senha,
+        setErro: setErroSenha,
+        mensagem: "Por favor, crie a sua senha.",
+      },
+      {
+        ref: phone,
+        setErro: setErroPhone,
+        mensagem: "Por favor, insira seu telefone.",
+      },
+      {
+        ref: rua,
+        setErro: setErroRua,
+        mensagem: "Por favor, insira o nome da sua rua.",
+      },
+      {
+        ref: bairro,
+        setErro: setErroBairro,
+        mensagem: "Por favor, insira o nome do seu bairro.",
+      },
+      { ref: cep, setErro: setErroCEP, mensagem: "Por favor, insira seu CEP." },
+      {
+        ref: cidade,
+        setErro: setErroCidade,
+        mensagem: "Por favor, insira o nome da sua cidade.",
+      },
+    ];
     const select = [
       {
         ref: estado,
         setErro: setErroEstado,
-        mensagem: "Por favor, selecione algum estado válido."
-      }
-    ]
-    if (verificarCampoVazio(camposObrigatorios, select)) return true
-     
+        mensagem: "Por favor, selecione algum estado válido.",
+      },
+    ];
+    if (verificarCampoVazio(camposObrigatorios, select)) return true;
+
     // tamanho limite
     const camposTamanhoMaximo = [
-      {ref: name,  limite: 70, setErro: setErroNome, mensagem: "O nome deve ter no máximo 70 caracteres. Por favor, insira um nome menor"},
-      { ref: email,  limite: 70, setErro: setErroEmail, mensagem: "O e-mail deve ter no máximo 70 caracteres. Por favor, insira um email menor"},
-      { ref: senha,  limite: 30, setErro: setErroSenha, mensagem: "A senha deve ter no máximo 30 caracteres. Por favor, insira uma senha menor"},
-      { ref: rua,  limite: 120, setErro: setErroRua, mensagem: "A rua deve ter no máximo 120 caracteres. Por favor, verifique se o nome da rua está correto."},
-      { ref: bairro,  limite: 120, setErro: setErroBairro, mensagem: "O bairro deve ter no máximo 120 caracteres. Por favor, verifique se o nome do bairro está correto."},
-      { ref: cidade,  limite: 35, setErro: setErroCidade, mensagem: "A cidade deve ter no máximo 35 caracteres. Por favor, verifique se o nome da cidade está correto."},
-    ]
-    if (verificarTamanhoMaximo(camposTamanhoMaximo)) return true
-    
+      {
+        ref: name,
+        limite: 70,
+        setErro: setErroNome,
+        mensagem:
+          "O nome deve ter no máximo 70 caracteres. Por favor, insira um nome menor",
+      },
+      {
+        ref: email,
+        limite: 70,
+        setErro: setErroEmail,
+        mensagem:
+          "O e-mail deve ter no máximo 70 caracteres. Por favor, insira um email menor",
+      },
+      {
+        ref: senha,
+        limite: 30,
+        setErro: setErroSenha,
+        mensagem:
+          "A senha deve ter no máximo 30 caracteres. Por favor, insira uma senha menor",
+      },
+      {
+        ref: rua,
+        limite: 120,
+        setErro: setErroRua,
+        mensagem:
+          "A rua deve ter no máximo 120 caracteres. Por favor, verifique se o nome da rua está correto.",
+      },
+      {
+        ref: bairro,
+        limite: 120,
+        setErro: setErroBairro,
+        mensagem:
+          "O bairro deve ter no máximo 120 caracteres. Por favor, verifique se o nome do bairro está correto.",
+      },
+      {
+        ref: cidade,
+        limite: 35,
+        setErro: setErroCidade,
+        mensagem:
+          "A cidade deve ter no máximo 35 caracteres. Por favor, verifique se o nome da cidade está correto.",
+      },
+    ];
+    if (verificarTamanhoMaximo(camposTamanhoMaximo)) return true;
+
     // tamanho fixo
     const camposTamanhoFixo = [
       {
-        ref:phone,
+        ref: phone,
         quantidade: 11,
         setErro: setErroPhone,
-        mensagem: "O telefone tem que ter exatamente 11 caracteres, verifique o número digitado"
+        mensagem:
+          "O telefone tem que ter exatamente 11 caracteres, verifique o número digitado",
       },
       {
         ref: cep,
         quantidade: 8,
         setErro: setErroCEP,
-        mensagem: "O cep tem que ter exatamente 8 digitos, verifique se inseriu corretamente"
-      }
-    ]
-    if (verificarTamanhoFixo(camposTamanhoFixo)) return true
+        mensagem:
+          "O cep tem que ter exatamente 8 digitos, verifique se inseriu corretamente",
+      },
+    ];
+    if (verificarTamanhoFixo(camposTamanhoFixo)) return true;
 
     // tamanho minimo
-    if (verificarTamanhoMinimo( senha, 6, setErroSenha, "A senha tem que conter no mínimo 6 caracteres."
-    )) return true
+    if (
+      verificarTamanhoMinimo(
+        senha,
+        6,
+        setErroSenha,
+        "A senha tem que conter no mínimo 6 caracteres."
+      )
+    )
+      return true;
 
     // tem letras?
     const camposNaoPodemTerLetras = [
       {
         ref: phone,
         setErro: setErroPhone,
-        mensagem: "O campo de telefone só pode ter números."
+        mensagem: "O campo de telefone só pode ter números.",
       },
       {
         ref: cep,
         setErro: setErroCEP,
-        mensagem: "O campo de cep só pode ter números."
+        mensagem: "O campo de cep só pode ter números.",
       },
-    ]
-    if (verificarSeTemLetras(camposNaoPodemTerLetras)) return true
+    ];
+    if (verificarSeTemLetras(camposNaoPodemTerLetras)) return true;
 
     // tem numeros?
     const camposNaoPodemTerNumeros = [
       {
         ref: name,
         setErro: setErroNome,
-        mensagem: "O campo de nome não pode conter números."
+        mensagem: "O campo de nome não pode conter números.",
       },
       {
         ref: bairro,
         setErro: setErroBairro,
-        mensagem: "O campo de bairro não pode conter números."
+        mensagem: "O campo de bairro não pode conter números.",
       },
       {
         ref: cidade,
         setErro: setErroCidade,
-        mensagem: "O campo de cidade não pode conter números."
-      }
-    ]
-    if (verificarSeTemNumeros(camposNaoPodemTerNumeros)) return true
-    
+        mensagem: "O campo de cidade não pode conter números.",
+      },
+    ];
+    if (verificarSeTemNumeros(camposNaoPodemTerNumeros)) return true;
+
     // tem @ ?
-    if (verificarSeEEmail(email, setErroEmail, "O e-mail precisa ter o @, verifique se digitou corretamente o seu e-mail")) return true
-    
+    if (
+      verificarSeEEmail(
+        email,
+        setErroEmail,
+        "O e-mail precisa ter o @, verifique se digitou corretamente o seu e-mail"
+      )
+    )
+      return true;
+
     const dados = {
       nome: nomeRef.current.value,
       email: emailRef.current.value,
@@ -156,12 +235,14 @@ function Cadastro() {
     };
 
     try {
-      await enviarDados(dados, "/form/cadastro-usuario");
-      console.log("dados enviados com sucesso")
+      const dadosAoBack = await enviarDados(
+        dados,
+        "http://localhost:3000/form/cadastro-usuario"
+      );
+      console.log("resposta do backend: ", dadosAoBack);
     } catch (error) {
-      console.log("erro ao enviar dados:", error)
+      console.log("erro ao enviar dados:", error);
     }
-
   }
 
   const UF = [
