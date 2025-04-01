2347706dfb81d5012c2fad5ea4eae204da3ca141
Israel de Souza <israellimas@hotmail.com>
2025-04-01 12:59:35 -0300

    feat: criado rota pro formulario de pet perdido

--- frontend/busca-pet/src/App.jsx
+++ frontend/busca-pet/src/App.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-
-import PetPerdido from "./pages/PetPerdido";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// PAGINAS
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
 import RecuperacaoSenha from "./pages/RecuperacaoSenha";
 import TelaPost from "./pages/TelaPost";
+import PetPerdido from "./pages/PetPerdido";
 import PageResearch from "./pages/PageResearch";
 
 
 function App() {
   return (
     <div className="App">
-
-
-
       <BrowserRouter>
         <Routes>
           <Route path="/form/cadastro-usuario" element={<Cadastro />} />
@@ -23,6 +20,7 @@ function App() {
           <Route path="/form/recuperacao-senha" element={<RecuperacaoSenha />} />
 
           <Route path="/posts/criar-post" element={<TelaPost />} />
+          <Route path="/form/post/pet-perdido" element={<PetPerdido />} />
           <Route path="/posts/pesquisa" element={<PageResearch />} />
         </Routes>
       </BrowserRouter>
