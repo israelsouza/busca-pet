d49edad2e4fc131dd8cbb18c160a3044d67ea83c
Israel de Souza <israellimas@hotmail.com>
2025-04-03 03:59:45 -0300

    feat: utilizando função que vai cadastrar usuario e removido excesso comentarios

--- backend/routes/cadastroUsuario.js
+++ backend/routes/cadastroUsuario.js
@@ -1,20 +1,15 @@
 import express from "express";
-// import inserirUsuarioBD from "../model/cadastrarUsuario.js";
+import inserirUsuarioBD from "../model/inserirUsuario.js";
 
-// metodo para que o express possua o poder de gerenciar as requisoes HTTP
 const router = express.Router();
 
 router.post("/", (request, response) => {
-  
-  const dados = request.body; // armazena os valores do formulario
-  response.status(200).json({ message: "Mensagem vinda do Backend: Dados enviados com sucesso" }); // responde que valores chegaram com sucesso [TODO NO FRONT]
+  const dados = request.body;
+  response
+    .status(200)
+    .json({ message: "Mensagem vinda do Backend: Dados enviados com sucesso" });
 
-  // verificar valores
-  console.log("Dados recebidos no backend ---> ", dados);
-  // chamar metodo que vai inserir valores no banco de dados
+  inserirUsuarioBD(dados);
 });
 
-
-// semelhante ao retorno dentro de uma função
-// ao chamar o arquivo 'cadastroUsuario.js' o valor dele ser essa exportação
 export default router;
