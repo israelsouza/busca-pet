b97ab9e693b8f4b866d8511bf04b2135df5163f9
sabrina1405 <sabrina.s.souzarosa@gmail.com>
2025-04-04 18:47:43 -0300

    Atualização home
    - Correção dos erros da Home;
    - Atualização do código da Main

--- frontend/busca-pet/src/pages/Home.jsx
+++ frontend/busca-pet/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo_Cachorro from "../assets/imgs/Logo_Cachorro.png";
 import style from "./styles/home.module.css";
 import ButtonHome from "../components/ButtonHome";
@@ -8,7 +8,48 @@ import AvaliacaoHome from "../components/AvaliacaoHome.jsx";
 import ComoFunciona from "../components/instrucoesHome.jsx";
 import abraco_dogHome from "../assets/imgs/abraco_dogHome.png";
 import footer_home from "../assets/imgs/footer_home.png";
+import { useEffect, useRef } from "react";
+
+
+
 function Home(){
+        const navigate = useNavigate();
+        const sessaoColaborar = useRef(null);
+        
+        // bloco 01
+        const sessaoReencontros = useRef(null);
+        useEffect(() => {
+            if (window.location.hash === "#reencontros") {
+                sessaoReencontros.current?.scrollIntoView({ behavior: "smooth" });
+            }
+        }, [window.location.hash]);
+        const irParaReencontros = () => {
+            navigate("#reencontros", { replace: true });
+            sessaoReencontros.current?.scrollIntoView({ behavior: "smooth" });
+          };
+
+
+        // bloco 02
+        const sessaoFuncionamento = useRef(null);
+        useEffect(() => {
+            console.log('entrei aqui')
+           if(window.location.hash === "#funcionamento"){
+              sessaoFuncionamento.current?.scrollIntoView({ behavior: "smooth" })
+          }
+          }, [window.location.hash]);
+       
+        const irParaFuncionamento = () => {
+            navigate("#funcionamento", { replace: true });
+            sessaoFuncionamento.current?.scrollIntoView({ behavior: "smooth" });
+          };
+
+
+
+          const irParaColabore = () => {
+            navigate("#colabore", { replace: true });
+            sessaoColaborar.current?.scrollIntoView({ behavior: "smooth" });
+          };
+      
     return (
             <>
             <div className={style.header}>
@@ -16,11 +57,10 @@ function Home(){
             <div className={style.container_header}>
                 <nav>
                     <ul className={style.ul}>
-                        <Link className={style.linkhome} to={''}>Início</Link>
-                        <Link  className={style.linkhome} to={''}>Reencontros</Link>
-                        <Link  className={style.linkhome} to={''}>Como Funciona</Link>
-                        <Link  className={style.linkhome} to={''}>Colabore</Link>
-
+    
+                        <Link  className={style.linkhome} onClick={irParaReencontros}>Reencontros</Link>
+                        <Link  className={style.linkhome} onClick={irParaFuncionamento}>Como Funciona</Link>
+                        <Link  className={style.linkhome} onClick={irParaColabore}>Colabore</Link>
                     </ul>
                 </nav>
                 <ButtonHome text_home="Cadastrar" path="/form/cadastro"/> 
@@ -34,16 +74,17 @@ function Home(){
             <p className={style.p}>Encontre seu pet perdido ou ajude a reunir famílias.</p>
             <p className={style.p}>Cadastre, busque e traga esperança para quem precisa!</p>
         </div>
-        <div>
+
+        <div id="reencontros" ref={sessaoReencontros}>
             <h2 className={style.h2}>Reencontros de sucesso</h2>
-            <div className={style.containerAvaliacoes}>
+            <div className={style.containerAvaliacoes} >
             <AvaliacaoHome image_perfil={Ellipse_home} usuario_text="Ana Paula, SP" avaliacao_text="Graças ao site, reencontrei meu cachorro em apenas dois dias! A comunidade é incrível e ajudou a espalhar a mensagem."/>
             <AvaliacaoHome image_perfil={Ellipse_home} usuario_text="Ana Paula, SP" avaliacao_text="Graças ao site, reencontrei meu cachorro em apenas dois dias! A comunidade é incrível e ajudou a espalhar a mensagem."/>
             <AvaliacaoHome image_perfil={Ellipse_home} usuario_text="Ana Paula, SP" avaliacao_text="Graças ao site, reencontrei meu cachorro em apenas dois dias! A comunidade é incrível e ajudou a espalhar a mensagem."/>
             </div>
         </div>
 
-        <div>
+        <div id="funcionamento" ref={sessaoFuncionamento}>
             <h3 className={style.h3}>Como Funciona</h3>
             <div>
             <ComoFunciona  text_instrucao="1 - Cadastre-se" text_explicacao="Crie uma conta em poucos passos e tenha acesso à nossa plataforma. Com seu cadastro, você poderá registrar pets perdidos ou encontrados e se conectar com outras pessoas que possam ajudar."/>
@@ -51,7 +92,8 @@ function Home(){
             <ComoFunciona  text_instrucao="3 - Fique de olho em seu e-mail" text_explicacao="Quando alguém encontrar um pet semelhante ao que você cadastrou ou quiser entrar em contato sobre um animal encontrado, você receberá uma notificação por e-mail. Fique atento para não perder nenhuma atualização!"/>
             </div>
         </div>
-        <div>
+
+        <div id="colabore" ref={sessaoColaborar}>
             <article className={style.article}>
                 <h4 className={style.h4}>Ajude Mais Pets!</h4>
                 <div className={style.article_text}>
