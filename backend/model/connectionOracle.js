70bd1c43c6cc660149906512be2eff73109aa333
Israel de Souza <israellimas@hotmail.com>
2025-04-04 23:14:09 -0300

    refactor: removed dotenv configuration and create a new file to save the config to connect to oracle

--- backend/model/connectionOracle.js
+++ backend/model/connectionOracle.js
@@ -1,16 +1,6 @@
 import OracleDB from "oracledb";
-import dotenv from "dotenv";
+import dbConfig from "../config/dbConfig.js";
 
-dotenv.config();
-
-// informações que vão ficar no .env para só os devs terem acesso ao banco
-const dbConfig = {
-  user: dotenv.DB_USER,
-  password: dotenv.DB_PASSWORD,
-  connectString: dotenv.DB_STRING_CONNECT,
-};
-
-// uma função que vai retornar a conexão com o banco quando for chamada em outro arquivo
 async function getConnection() {
   try {
     return await OracleDB.getConnection(dbConfig);
