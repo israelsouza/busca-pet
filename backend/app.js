08bfd052eb1e25f15cc8f69c95620acc0a0503f6
Israel de Souza <israellimas@hotmail.com>
2025-04-08 19:47:42 -0300

    feat: criação da rota da página de todos os posts, usando o middleware + usando rota de validação + imports

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import cadastroUsuario from "./routes/cadastroUsuario.js";
-import logarUsuario from './routes/logarUsuario.js'
+import logarUsuario from "./routes/logarUsuario.js";
+import getTodosPosts from "./routes/getTodosPosts.js";
+import autenticarToken from "./middleware/authMiddleware.js";
+import validateToken from "./routes/validateToken.js";
 
 const app = express();
 app.use(cors());
@@ -9,8 +12,15 @@ app.use(express.json());
 
 // app vai usar as rotas definidas abaixo
 // as rotas vao conter todo fluxo de execução, podendo realizar chamadas de outras funções (model)
+
+// rotas publicas
 app.use("/form/cadastro-usuario", cadastroUsuario);
 app.use("/form/login", logarUsuario);
 
+// rota de verificação do token
+app.use("/validate-token", validateToken);
+
+// rotas privadas (protegidas)
+app.use("/posts/all", autenticarToken, getTodosPosts);
 
 export default app;
