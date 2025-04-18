614f30152ca4779d0c662a53dbf96bcb7ddff9f4
sabrina1405 <sabrina.s.souzarosa@gmail.com>
2025-04-18 20:32:15 -0300

    Finalização da Tela de Visualizar perfil
    - Estilização Completa;
    - Adicionar os links nas ancoras;

--- frontend/busca-pet/src/pages/PerfilVisualizar.jsx
+++ frontend/busca-pet/src/pages/PerfilVisualizar.jsx
@@ -6,13 +6,13 @@ import icon_publicacoes from "../assets/imgs/icon_publicacoes.png";
 import avatar_usuario from "../assets/imgs/avatar_usuario.png";
 import ButtonSection from "../components/ButtonSection";
 import Style from "../pages/styles/PerfilVisualizar.module.css";
+import fundo_pato from "../assets/imgs/fundo_pato.png";
 function VisualizePerfil(){    
         return <div>
             <HeaderLog />
-                <div className="FotoDivisao">
-
-                </div>
-                <section>
+            <div className={Style.container}>
+            <div className={Style.FotoDivisao}></div>
+                <section className={Style.perfilSection}>   
                     <article className={Style.cabecalho}>
                     <img src={avatar_usuario} alt="icone de foto de usuário" />
                     <div className={Style.namecontainer}>
@@ -20,15 +20,14 @@ function VisualizePerfil(){
                     <h2 className={Style.h2}>Nome Exemplo do Usuário</h2>
                     </div>
                     </article>
-                    <article>
                     <BotaoSection img_icone={icon_conta} nome_section="Conta" alt="Icone de perfil" text_section="Informações de contato e endereço." acesso="" />
-                    </article>
-                        <BotaoSection img_icone={icon_notificacoes} nome_section="Notificações" alt="Icone de notificações" text_section="Informações Veja quem interagiu com sua publicação." acesso=""/>
-                  
-                    <article>
-                        <BotaoSection img_icone={icon_publicacoes} nome_section="Publicações" alt="Icone de imagens" text_section="Veja suas próprias publicações." acesso=""/>
-                    </article>
+    
+                    <BotaoSection img_icone={icon_notificacoes} nome_section="Notificações" alt="Icone de notificações" text_section="Informações Veja quem interagiu com sua publicação." acesso=""/>
+
+                    <BotaoSection img_icone={icon_publicacoes} nome_section="Publicações" alt="Icone de imagens" text_section="Veja suas próprias publicações." acesso=""/>
                 </section>
+                
+            </div>
 
             </div>
 }
