61c07bcb68a423b91b47643f8aa7fbefe34d1cde
Israel de Souza <israellimas@hotmail.com>
2025-04-25 20:50:02 -0300

    feat: tratamento de excessões e retorno ao frontend

--- backend/controllers/atualizarSenhaController.js
+++ backend/controllers/atualizarSenhaController.js
@@ -13,18 +13,26 @@ async function atualizarSenhaController(req, res) {
         const salt = await bcrypt.genSalt(saltRounds);
         const newPassword = await bcrypt.hash(password, salt);
             
-        await updatePassword(newPassword, email);
+        const senhaAtualizada = await updatePassword(newPassword, email);
+
+        if(!senhaAtualizada)
+            res.status(400).send({message: "Não foi possível atualizar a senha."})
 
         console.log("Sucesso, senha atualizada!!");
 
         // remover token banco de dados
 
         const idUser = await getUserIdByEmail(email)
-        await removeTokenData(idUser)
+        const remocaoTokenBanco = await removeTokenData(idUser)
 
+        if(!remocaoTokenBanco)
+            res.status(400).send({ message: "Não foi possível remover o token do banco de dados." });
+
+        return res.status(200).send({ message: "Sucesso, senha atualizada. Realize o login." });
 
     } catch (error) {
         console.log(error)
+        return res.status(400).send({message: "Erro ao tentar atualizar a senha. Tente novamente"})
     }
 }
 
