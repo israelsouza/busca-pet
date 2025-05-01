33c487bc17acb8477d18223206f7ffb756acf972
Israel de Souza <israellimas@hotmail.com>
2025-05-01 16:45:17 -0300

    feat: add função para realizar as demais requisições ao backend

--- frontend/busca-pet/src/components/HeaderLog.jsx
+++ frontend/busca-pet/src/components/HeaderLog.jsx
@@ -12,9 +12,9 @@
                 <img className={style.logodog} src={Logo_Cachorro} alt="Logo com um cachorro peludo usando uma lupa" width="150px"/>
             </Link>
                 <nav className={style.navegation}>
-                    <Link to={'/posts/all?category=all'} className={style.links_header}>Todos</Link>
-                    <Link to={'/posts/all?category=lost'} className={style.links_header}>Achados</Link>
-                    <Link to={'/posts/all?category=found'} className={style.links_header}>Perdidos</Link>
+                    <Link to={'/posts/all?category=all'} onClick={() => onSelectCategory('all')}  className={style.links_header}>Todos</Link>
+                    <Link to={'/posts/all?category=found'} onClick={() => onSelectCategory('found')} className={style.links_header}>Achados</Link>
+                    <Link to={'/posts/all?category=lost'} onClick={() => onSelectCategory('lost')} className={style.links_header}>Perdidos</Link>
                     <Link to={'/posts/pesquisa'} className={style.links_header}>Pesquisar</Link>
                 </nav>
                 
