433d38c854c6d22fa5445e60a8f487416c944b3a
Israel de Souza <israellimas@hotmail.com>
2025-05-02 18:39:00 -0300

    feat: add email extraction from token in getUserPosts function

--- backend/controllers/PostControler.js
+++ backend/controllers/PostControler.js
@@ -1,6 +1,7 @@
 import getTodosPosts from "../model/getTodosPosts.js";
 import getTipoPostModel from "../model/getTipoPostModel.js";
 import getUserPostsModel from "../model/getUserPost.js";
+import extrairEmailDoToken from "../utils/extrairEmailDoToken.js";
 
 async function todosPosts(req, res) {
   try {
@@ -31,12 +32,10 @@ async function getPostEncontrado(req, res) {
   try {
     const tipo = "Encontrado";
     const posts = await getTipoPostModel(tipo);
-    return res
-      .status(200)
-      .json({
-        message: "Posts de pet encontrados capturados com sucesso",
-        posts,
-      });
+    return res.status(200).json({
+      message: "Posts de pet encontrados capturados com sucesso",
+      posts,
+    });
   } catch (error) {
     console.error("getTipoPost encontrado error: ", error);
     return res.status(500).json({ error: error.message });
@@ -44,8 +43,10 @@ async function getPostEncontrado(req, res) {
 }
 
 async function getUserPosts(req, res) {
-  const { email } = req.body; // ID do usuário via corpo da requisição
+  const { token } = req.body;
   try {
+    const email = await extrairEmailDoToken(token);
+    // console.log("email do token: ", email);
     const posts = await getUserPostsModel(email);
     return res
       .status(200)
