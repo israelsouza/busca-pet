11fe9f7f13670c00969ad0c644b24722bfbfa8bf
sabrina1405 <sabrina.s.souzarosa@gmail.com>
2025-05-28 17:43:04 -0300

    correção

--- frontend/busca-pet/src/pages/PerfilVisualizar.jsx
+++ frontend/busca-pet/src/pages/PerfilVisualizar.jsx
@@ -54,7 +54,7 @@ function VisualizePerfil({userId}){
                 <section className={Style.perfilSection}>   
                     <article className={Style.cabecalho}>
             
-                    <img src={`data:image/jpeg;base64,${userInfo.USU_FOTO}`} width="200px" height="100px" alt="icone de foto de usuário" />
+                    <img src={`data:image/jpeg;base64,${userInfo.USU_FOTO}`} width="200px" alt="icone de foto de usuário" />
                        
                     <div className={Style.namecontainer}>
                     <h2 className={Style.h2}>{userInfo.PES_NOME || "Exemplo de nome de Usuário"}</h2>
