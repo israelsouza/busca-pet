5e41e95c814f5d6ec9bd6173de976f2239db3175
Israel de Souza <israellimas@hotmail.com>
2025-05-28 04:04:25 -0300

    chore: config global da lib MAPS

--- frontend/busca-pet/src/App.jsx
+++ frontend/busca-pet/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoadScript } from '@react-google-maps/api';
 
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
@@ -14,10 +15,37 @@ import EditarPerfil from "./pages/EditPerfil";
 import PostsUser from "./pages/PostsUser";
 import Notification from "./pages/Notification";
 
+import API_KEY from './config/maps-api.js';
+
+const libraries = ['places', 'geometry']
+
 function App() {
+
+  if(!API_KEY) {
+    return (
+      <div style={{ textAlign: 'center', padding: '50px', color: 'red' }}>
+        <h1>Erro de Configuração</h1>
+        <p>A chave da API do Google Maps não está configurada.</p>
+        <p>Por favor, adicione `REACT_APP_Maps_API_KEY` ao seu arquivo .env e reinicie o servidor de desenvolvimento.</p>
+      </div>
+    )
+  }
+
+
+
   return (
     <div className="App">
       <BrowserRouter>
+
+        <LoadScript 
+          googleMapsApiKey={API_KEY}
+          libraries={libraries}
+          loadingElement={<div style={{ textAlign: 'center', padding: '50px' }}>Carregando Google Maps...</div>}
+          onError={(error) => {
+            console.error("Erro ao carregar Google Maps Script:", error);
+          }}
+        >
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/form/cadastro-usuario" element={<Cadastro />} />
@@ -42,6 +70,8 @@ function App() {
           <Route path="/Posts/user" element={<PostsUser />} />
           <Route path="/user/notificacao" element={<Notification />} />
         </Routes>
+
+        </LoadScript>
       </BrowserRouter>
     </div>
   );
