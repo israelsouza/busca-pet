5fb38e96056e37fd6d99f4652ddd89c6c934927f
Israel de Souza <israellimas@hotmail.com>
2025-06-20 00:23:48 -0300

    refactor: remove arquivos inutilizados apos refatoração

--- backend/controllers/minhasPublicacoes.js
+++
@@ -1,5 +0,0 @@
-async function getMinhasPublicacoes(req, res) {
-    console.log("OLAAAAAAAAAA ")
-}
-
-export default getMinhasPublicacoes
--- backend/routes/minhasPublicacoes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import getMinhasPublicacoes from "../controllers/minhasPublicacoes.js";
-
-const router = express.Router();
-
-router.get("/", getMinhasPublicacoes)
-
-export default router;
