cab75db0feca7ef3e094860dca03817323a345a3
Israel de Souza <israellimas@hotmail.com>
2025-06-21 18:53:19 -0300

    refactor: remove arquivos não utilizados

--- backend/routes/denuncia.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import DenunciaController from '../controllers/denunciaController.js'; // Ajuste o nome do import
-import authenticateToken from '../middleware/authMiddleware.js'; // Ajuste se o nome da exportação for diferente
-
-const router = express.Router();
-
-router.post('/denunciar', authenticateToken, DenunciaController.criarDenuncia);
-
-export default router;
