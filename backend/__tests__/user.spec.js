aab7123f95338573339b6bcef14c97f78826a94f
Israel de Souza <israellimas@hotmail.com>
2025-06-24 23:04:45 -0300

    feat: teste de sucesso e fracasso para api/usuario/solicitar-nova-senha

--- backend/__tests__/user.spec.js
+++ backend/__tests__/user.spec.js
@@ -1,11 +1,13 @@
 import request from 'supertest';
 import app from '../app.js';
 
-describe( "POST /api/usuario/cadastro", () => {
+const API_USUARIO = '/api/usuario'
+
+describe( `POST ${API_USUARIO}/cadastro`, () => {
     test('deve cadastrar usuário com sucesso', async () => { 
         const unique = Date.now();
         const response = await request(app)
-            .post('/api/usuario/cadastro')
+            .post(`${API_USUARIO}/cadastro`)
             .send({
                 nome: "João Paulo", 
                 email: `joaozinho${unique}@teste.com.br`, 
@@ -25,7 +27,7 @@ describe( "POST /api/usuario/cadastro", () => {
         const unique = Date.now();
         const error = {};
         const response = await request(app)
-            .post('/api/usuario/cadastro')
+            .post(`${API_USUARIO}/cadastro`)
             .send({
                 nome: "João Paulo", 
                 email: `joaozinho1234@teste.com.br`, 
@@ -43,10 +45,10 @@ describe( "POST /api/usuario/cadastro", () => {
 
 } )
 
-describe( "POST /api/usuario/login", () => {
+describe( `POST ${API_USUARIO}/login`, () => {
     test('should login an user', async () => {
         const emailTesting = "raquel@teste.com";
-        const response = await request(app).post('/api/usuario/login').send({
+        const response = await request(app).post(`${API_USUARIO}/login`).send({
             email: emailTesting,
             password: '123123'
         })
@@ -64,12 +66,29 @@ describe( "POST /api/usuario/login", () => {
 
     test('should throw a error when try to login an user', async () => {
         const emailTesting = "raquel@teste.com";
-        const response = await request(app).post('/api/usuario/login').send({
+        const response = await request(app).post(`${API_USUARIO}/login`).send({
             email: emailTesting,
             password: '12'
         })
         expect(response.status).toBe(500)
         expect(response.body).toEqual({message: "Erro interno do servidor", error: {}})
+    })
+})
+
+describe( `POST ${API_USUARIO}/solicitar-nova-senha`, () => {
+    test('deve criar token e enviar ao email com sucesso', async () => {
+        const email = "israellimas@hotmail.com";
+        const response = await request(app).post(`${API_USUARIO}/solicitar-nova-senha`).send({
+            email: email
+        })
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual({message: "Email enviado! Verifique a sua caixa de spam, se necessário", success: true})
+    })
+
+    test('deve lançar erro ao tentar criar token e enviar ao email', async () => {
+        const response = await request(app).post(`${API_USUARIO}/solicitar-nova-senha`).send({})
+        expect(response.status).toBe(500)
+        expect(response.body).toEqual({ error: "Ocorreu um erro interno. Por favor, tente novamente mais tarde." })
     })
 })
 
