6a0f32856e1ab50a8c1ca9fda3f58b4b23ed9f58
Israel de Souza <israellimas@hotmail.com>
2025-06-26 18:48:21 -0300

    feat: adiciona teste para a criação de uma notificação

---
+++ backend/__tests__/notific.spec.js
@@ -0,0 +1,37 @@
+import request from 'supertest';
+import app from '../app.js';
+import { gerarAuthToken } from './utils/auth.js';
+
+const API_NOTIFIC = '/api/notificacao';
+
+let token;
+
+beforeAll(async () => {
+    token = await gerarAuthToken();
+});
+
+
+describe(` POST ${API_NOTIFIC}/criar/mensagem`, () => {
+    test("deve criar uma notificação ", async () => {
+        const response = await request(app)
+            .post(`${API_NOTIFIC}/criar/mensagem`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({idPost: 300})
+        
+        expect(response.status).toEqual(201)
+        expect(response.body).toMatchObject({
+            message: "Mensagem enviada com sucesso."
+        })
+    })
+
+    test("deve retornar ERRO sem enviar o ID do POST ", async () => {
+        const response = await request(app)
+            .post(`${API_NOTIFIC}/criar/mensagem`)
+            .set('Authorization', `Bearer ${token}`)
+        
+        expect(response.status).toEqual(400)
+        expect(response.body).toMatchObject({
+            message: "Erro ao enviar mensagem."
+        })
+    })
+})
